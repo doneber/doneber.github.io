53a92f19c6a5b245d453d6e45fb2625c17920adc
Eber <doneber98@gmail.com>
2024-02-23 11:42:37 -0400

    agrega i18n y configura ui nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,14 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://doneber.dev',
+  i18n: {
+    defaultLocale: 'es',
+    locales: [ 'es', 'en' ],
+    routing: {
+      prefixDefaultLocale: false
+    },
+    fallback: {
+      en: 'es'
+    }
+  }
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const languages = {
+  es: 'Español',
+  en: 'English',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+  es: {
+    'nav.home':'Inicio',
+    'nav.blog':'Blog',
+    'nav.portfolio':'Portafolio',
+    'nav.about':'Sobre mí',
+    'nav.setup':'Mi Setup'
+  },
+  en: {
+    'nav.home':'Home',
+    'nav.blog':'Blog',
+    'nav.portfolio':'Portfolio',
+    'nav.about':'About me',
+    'nav.setup':'My Setup'
+  },
+} as const;
