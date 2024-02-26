ef84b3ef4237279316986d9e265dc825e485c291
Eber <doneber98@gmail.com>
2024-02-26 17:06:54 -0400

    agrega rss

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,19 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const blog = await getCollection('blog');
+  return rss({
+    title: 'Doneber.dev | Blog',
+    description: 'Mi blog personal',
+    site: context.site,
+    // items: await pagesGlobToRssItems(import.meta.glob('@/content/blog/**/*.md')),
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+    })),
+    customData: `<language>en-us</language>`,
+  });
+}
