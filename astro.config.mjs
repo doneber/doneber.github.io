f05afbba164a6951215b97c0c623ca3185acc233
Eber <doneber.dev@gmail.com>
2026-01-14 14:35:48 -0400

    chore: upgrade to astro 6-beta

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://doneber.dev',
+
+  // TODO: update this to use updated blog content config
+  legacy: {
+    collectionsBackwardsCompat: true,
+  },
   i18n: {
     defaultLocale: 'es',
-    locales: [ 'es', 'en' ],
+    locales: ['es', 'en'],
     routing: {
       prefixDefaultLocale: false
     },
