857aafbfd566e3f48a4286f0677894c6ca2ca5e1
Eber <doneber.dev@gmail.com>
2026-01-29 11:50:55 -0400

    feat: Add new blog post, integrate custom Comic Shanns font, and update image component usage.

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,13 +5,13 @@ import { glob } from 'astro/loaders';
 const blog = defineCollection({
 	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/blog" }),
 	// Type-check frontmatter using a schema
-	schema: z.object({
+	schema: ({ image }) => z.object({
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		heroImage: image().optional(),
 		draft: z.boolean().optional(),
 		author: z.string().optional(),
 		tags: z.array(z.string()).optional(),
