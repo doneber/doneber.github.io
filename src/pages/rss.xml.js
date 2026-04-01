9ef0fe8a114431cbd14999627f3ee7146c35a2ab
Eber <doneber.dev@gmail.com>
2026-04-01 11:23:47 -0400

    feat: improve blog list of articles & old post

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,11 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-  const blog = await getCollection('blog');
+  const blog = await getCollection('blog', ({ data }) => {
+    return import.meta.env.PROD ? data.draft !== true : true;
+  });
+
   return rss({
     title: 'Doneber.dev | Blog',
     description: 'Mi blog personal',
